c3bef3f1cd876f98446137547baa9260d543971d
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2022-05-10 14:30:30 +0700

    refactor

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:intl/intl.dart';
 //import './login.dart';
-import './script.dart';
+import './videoQuestion.dart';
 
 // ignore: must_be_immutable
 class MainScreen extends StatelessWidget {
@@ -104,12 +103,13 @@ class MainScreen extends StatelessWidget {
   }
 
   Widget _buildList() {
+    final videos = VideoQuestion().videoQuestions;
     return ListView.builder(
-        itemCount: scripts.length,
+        itemCount: videos.length,
         itemBuilder: (BuildContext context, int index) {
           return Column(
             children: [
-              _tile(scripts[index].title, scripts[index].subTitle),
+              _tile(videos[index].title, videos[index].subTitle),
               const Divider(
                 height: 20,
                 thickness: 1,
@@ -133,107 +133,4 @@ class MainScreen extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward),
     );
   }
-
-  List<Script> scripts = [
-    Script(
-      id: '1',
-      title: 'CineArts at the Empire',
-      subTitle: '85 W Portal Ave',
-    ),
-    Script(
-      id: '2',
-      title: 'The Castro Theater',
-      subTitle: '429 Castro St',
-    ),
-    Script(
-      id: '3',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '4',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '5',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '6',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '7',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '8',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '9',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '10',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '11',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '12',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '13',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '14',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '15',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '16',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '17',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '18',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '19',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-    Script(
-      id: '20',
-      title: 'Alamo Drafthouse Cinema',
-      subTitle: '2550 Mission St',
-    ),
-  ];
 }
---
+++ lib/videoQuestion.dart
@@ -0,0 +1,106 @@
+import './script.dart';
+
+class VideoQuestion {
+  List<Script> videoQuestions = [
+    Script(
+      id: '1',
+      title: 'CineArts at the Empire',
+      subTitle: '85 W Portal Ave',
+    ),
+    Script(
+      id: '2',
+      title: 'The Castro Theater',
+      subTitle: '429 Castro St',
+    ),
+    Script(
+      id: '3',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '4',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '5',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '6',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '7',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '8',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '9',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '10',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '11',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '12',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '13',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '14',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '15',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '16',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '17',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '18',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '19',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+    Script(
+      id: '20',
+      title: 'Alamo Drafthouse Cinema',
+      subTitle: '2550 Mission St',
+    ),
+  ];
+}

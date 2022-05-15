3495aa47b15752f0cad647d7f2a391da7d9a3f0a
Anh Nguyen <tuananh3@gmail.com>
2022-05-15 18:44:17 +0700

    change data model

--- lib/script.dart
+++ lib/script.dart
@@ -4,11 +4,19 @@ class Script {
   final int id;
   final String title;
   final String subTitle;
+  final String first_name;
+  final String last_name;
+  final String email;
+  final String img;
 
   Script({
     @required this.id,
     @required this.title,
     @required this.subTitle,
+    @required this.first_name,
+    @required this.last_name,
+    @required this.email,
+    @required this.img,
   });
 
   factory Script.fromJson(Map<String, dynamic> json) {
@@ -16,6 +24,10 @@ class Script {
       id: json['id'] as int,
       title: json['title'] as String,
       subTitle: json['subTitle'] as String,
+      first_name: json['first_name'] as String,
+      last_name: json['last_name'] as String,
+      email: json['email'] as String,
+      img: json['img'] as String,
     );
   }
 }

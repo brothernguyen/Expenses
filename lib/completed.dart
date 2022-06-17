03040a71fc48b5342456c1d744c701269f2c36b2
brothernguyen <tuananh3@gmail.com>
2022-06-17 19:35:56 +0700

    completed scripts (cont.)

--- lib/completed.dart
+++ lib/completed.dart
@@ -1,48 +1,26 @@
+import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_complete_guide/auth.dart';
-import './completed.dart';
-import './login.dart';
-import './script.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import './cardDetail.dart';
-import './pickImage.dart';
+import './script.dart';
 
-// ignore: must_be_immutable
-class MainScreen extends StatefulWidget {
-  MainScreen({Key key}) : super(key: key);
+class Completed extends StatefulWidget {
+  const Completed({Key key}) : super(key: key);
 
   @override
-  MainScreenState createState() {
-    return MainScreenState();
-  }
+  State<Completed> createState() => _CompletedState();
 }
 
-class MainScreenState extends State<MainScreen> {
+class _CompletedState extends State<Completed> {
   List<Script> items = [];
   bool isLoading = false;
-  bool isWifiOnly = false;
-  bool isHD = false;
-  int _selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
     fetchScripts();
-  }
-
-  @override
-  void didUpdateWidget(MainScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() {
-    print('dispose()');
-    super.dispose();
   }
 
   Future<void> fetchScripts() async {
@@ -84,125 +62,15 @@ class MainScreenState extends State<MainScreen> {
     }
   }
 
-  Future<void> _signOut(BuildContext context) async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.pop(context);
-    Navigator.pushNamed(context, '/');
-  }
-
   @override
   Widget build(BuildContext context) {
-    String appBarTitle =
-        _selectedIndex == 0 ? 'Data Question' : 'Completed Scripts';
-    final PreferredSizeWidget appBar =
-        AppBar(title: Text(appBarTitle), actions: <Widget>[
-      IconButton(
-        icon: const Icon(Icons.power_settings_new),
-        tooltip: 'Log out',
-        onPressed: () {
-          _signOut(context);
-        },
-      ),
-    ]);
-
     return Scaffold(
-      appBar: appBar,
-      drawer: Container(
-          width: MediaQuery.of(context).size.width * 0.9, child: _drawer()),
       body: RefreshIndicator(
         onRefresh: () async {
           await Future.delayed(Duration(seconds: 2));
           fetchScripts();
         },
-        //child: Center(child: _buildList(context)),
-        child: _tabBarItems(context, _selectedIndex),
-      ),
-      bottomNavigationBar: _bottomBar(),
-    );
-  }
-
-  Widget _tabBarItems(BuildContext context, int _selectedIndex) {
-    List<Widget> widgetOptions = <Widget>[
-      Center(child: _buildList(context)),
-      Completed(),
-    ];
-    return widgetOptions.elementAt(_selectedIndex);
-  }
-
-  Widget _bottomBar() {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.file_copy_outlined),
-          label: 'Active',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.check_sharp),
-          label: 'Completed',
-        ),
-      ],
-      selectedItemColor: Colors.amber[800],
-      currentIndex: _selectedIndex,
-      onTap: onBottomBarTapped,
-    );
-  }
-
-  void onBottomBarTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  Widget _drawer() {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            child: Image.asset(
-              'assets/images/logo-vidlet-butterfly.png',
-              scale: 1.2,
-            ),
-          ),
-          ListTile(
-            trailing: Switch.adaptive(
-                value: isWifiOnly,
-                onChanged: (value) {
-                  setState(() {
-                    isWifiOnly = !isWifiOnly;
-                  });
-                }),
-            title: Text('WiFi only'),
-          ),
-          ListTile(
-            trailing: Switch.adaptive(
-                value: isHD,
-                onChanged: (value) {
-                  setState(() {
-                    isHD = !isHD;
-                  });
-                }),
-            title: Text('HD Video'),
-          ),
-          ListTile(
-            title: Text('Support'),
-          ),
-          ListTile(
-            title: Text('Private Policy'),
-          ),
-          ListTile(
-            title: Text('Logout'),
-            onTap: () async {
-              await Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => Auth()),
-              );
-            },
-          ),
-        ],
+        child: Center(child: _buildList(context)),
       ),
     );
   }
@@ -220,7 +88,7 @@ class MainScreenState extends State<MainScreen> {
               return Column(
                 children: [
                   GestureDetector(
-                    onTap: () => {onCardTapped(context, items[index])},
+                    // onTap: () => {onCardTapped(context, items[index])},
                     child: _card(index),
                   ),
                 ],
@@ -228,19 +96,10 @@ class MainScreenState extends State<MainScreen> {
             });
   }
 
-  onCardTapped(context, Script item) {
-    if (!item.isCompleted) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => CardDetail(item)),
-      );
-    }
-  }
-
   Card _card(int index) {
     double elevation = Platform.isAndroid ? 16 : 0;
     bool isCompleted = items[index].isCompleted;
-    if (isCompleted) {
+    if (!isCompleted) {
       return Card(
         child: Container(
           height: 0,

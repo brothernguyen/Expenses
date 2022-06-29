ac60016b4aa2f3366a7eebf9a2207e6fa650aa24
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2022-06-29 15:05:48 +0700

    added Chewie player

---
+++ lib/chewie_list_item.dart
@@ -0,0 +1,17 @@
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+
+class ChewieListItem extends StatefulWidget {
+  const ChewieListItem({Key key}) : super(key: key);
+
+  @override
+  State<ChewieListItem> createState() => _ChewieListItemState();
+}
+
+class _ChewieListItemState extends State<ChewieListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
